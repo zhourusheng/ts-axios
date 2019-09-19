1fe71185443c5208afe2be238a069699dffc0604
zhourusheng <nfuzrs@126.com>
2019-09-19 11:57:32 +0800

    headers

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -2,68 +2,87 @@ import axios from '../../src/index'
 
 // GET
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: ['bar', 'baz']
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: ['bar', 'baz']
+//   }
+// })
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: {
-      bar: 'baz'
-    }
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
 
-const date = new Date()
+// const date = new Date()
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    date
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     date
+//   }
+// })
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: '@:$, '
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: 'bar',
-    baz: null
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
 
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
 
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
-    bar: 'baz'
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     bar: 'baz'
+//   }
+// })
 
 // POST
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+
+// const arr = new Int32Array([21, 31])
+
+// axios({
+//   method: 'post',
+//   url: '/base/buffer',
+//   data: arr
+// })
+
+
+// Headers
 axios({
   method: 'post',
   url: '/base/post',
@@ -73,10 +92,23 @@ axios({
   }
 })
 
-const arr = new Int32Array([21, 31])
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
 
 axios({
   method: 'post',
-  url: '/base/buffer',
-  data: arr
+  url: '/base/post',
+  data: searchParams
 })
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,20 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
+
+  // 传入的 data 为空的时候，请求 header 配置 Content-Type 是没有意义的，于是我们把它删除。
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.send(data)
 }
